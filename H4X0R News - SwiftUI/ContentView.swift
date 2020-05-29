1535e69b87c81881096694e7490164b541ded2fb
Mayur <mayur@vulpe.in>
2020-05-29 07:13:36 +0530

    navigation view done.

--- H4X0R News - SwiftUI/ContentView.swift
+++ H4X0R News - SwiftUI/ContentView.swift
@@ -10,12 +10,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+             
+            List(posts) { post in
+                
+                
+                //In the this closure
+                //for every single post in the posts array
+                //we are going to use each of this Text objects
+                
+                Text(post.title)
+                //this text view will be populated by the .title
+                //property in the posts array
+            }
+                
+            
+             .navigationBarTitle("H4X0R News")
+            
+        }
+   
+        
+        
     }
 }
 
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone 11")
     }
 }
+
+
+
+struct Post: Identifiable {
+    let id : String
+    let title : String
+}
+
+let posts = [
+Post(id: "1", title: "THis is awesome"),
+Post(id: "2", title: "THis is more awesome"),
+    Post(id: "3", title: "THis is even more awesome")
+]
+
---
+++ H4X0R News - SwiftUI/Models/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  H4X0R News - SwiftUI
+//
+//  Created by Angela on 29/05/20.
+//  Copyright © 2020 Angela. All rights reserved.
+//
+
+import Foundation
